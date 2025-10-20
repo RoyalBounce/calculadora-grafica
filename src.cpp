17f8f2d000c6416f6d51526fd2a19690d3d10a46
Blink <137730237+RoyalBounce@users.noreply.github.com>
2025-10-20 16:25:05 +0000

    começo

---
+++ src.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using std::string
+struct functions{
+
+}
+
+double lim(double x, double y){
+    while (x < y){
+        x++
+    }
+    return x;
+}
+
+int main(int argc, char* argv[]){}
